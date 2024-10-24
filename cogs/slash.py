55ee02bfd3591bbb5cfadf17ea4592af4bcd9d73
watercup0926 <watercup0926@gmail.com>
2024-10-24 21:52:16 +0800

    add picture function
    叮哥搞出來了

--- cogs/slash.py
+++ cogs/slash.py
@@ -1,47 +1,46 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+
 """
 名字
 中杯
 大杯
 不可調甜度 做熱飲 不可調冰塊
 """
+
 import os
-store = ''
 def set_shop(name:str):
     file = open("{}.txt".format(name),'r',encoding="utf-8")
     lines = file.readlines()
     ls = []
-    menu = ''
-    for i in range(0,len(lines),4):
+    for i in range(1,len(lines),4):
         ls.append([lines[i],int(lines[i+1]),int(lines[i+2]),list(lines[i+3])])
-    for i in range(0,len(ls)):
-        menu += "{} 中杯{} 大杯{} ".format(ls[i][0],ls[i][1],ls[i][2])
-        if ls[i][3][0] == '1':
-            menu += "不可調甜度 "
-        if ls[i][3][1] == '1':
-            menu += "可做冷飲 "
-        if ls[i][3][0] == '1':
-            menu += "不可調冰塊"
-        menu += '\n'
-    return menu
+    return ls
+def set_menu(name:str):
+    file = open("{}.txt".format(name),'r',encoding="utf-8")
+    line = file.readline()
+    return line
 class Slash(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-
-    # Define a slash command
-    @app_commands.command(name="菜單", description="列出菜單的所有項目")
+        self.shop_name = ''
+        self.shop_list = ''
+        self.shop_menu = ''
+        # Define a slash command
+    @app_commands.command(name="菜單", description="給出今天的菜單")
     async def menu(self, interaction: discord.Interaction):
-        #if store != '': 
-        await interaction.response.send_message(set_shop(store),ephemeral=True)
-        #else:
-        #    await interaction.response.send_message("還沒決定哪間")
+        if self.shop_name != '': 
+            await interaction.response.send_message(self.shop_menu,ephemeral=True)
+        else:    
+            await interaction.response.send_message("還沒決定哪間")
     @app_commands.command(name="今日店家",description="今天要喝哪家呢?")
     async def store(self, interaction: discord.Interaction,店家:str):
-        store = 店家
-        await interaction.response.send_message("各位，今天喝{}喔\n{}".format(store,set_shop(store)))
-    
+        self.shop_name=店家
+        self.shop_list=set_shop(店家)
+        self.shop_menu=set_menu(店家)
+        await interaction.response.send_message("各位，今天喝{}喔{}".format(店家,self.shop_menu))
+
 # Cog setup function
 async def setup(bot: commands.Bot):
     await bot.add_cog(Slash(bot))
