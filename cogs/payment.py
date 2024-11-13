4ccb80327f56d05c9a14fdf38a35f2075a852134
watercup0926 <watercup0926@gmail.com>
2024-11-13 13:52:57 +0800

    Revert "Update payment.py"
    This reverts commit e631ac6a222dd6a513c7cf88d4c92d3dba30ccc2.

--- cogs/payment.py
+++ cogs/payment.py
@@ -7,7 +7,8 @@ from discord.ext import commands
 
 def read():
     with open("user_data.json", "r", encoding="UTF-8") as file:
-        return json.load(file)
+        data = json.load(file)
+    return data
 
 
 def write(data):
@@ -20,15 +21,20 @@ class Payment(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="檢查存款", description="檢查餘額")
-    async def pay(self, interaction: discord.Interaction):
+    @app_commands.commands.command()
+    async def pay(self, ctx, amount: int, member: discord.Member):
         data = read()
-        user = interaction.user.id
-        if user not in data:
-            await interaction.response.send_message("你還沒有帳戶，創建一個", ephemeral=True)
-            data[user] = 0
-            write(data)
-        await interaction.response.send_message(f"你剩 {data[user]} 元", ephemeral=True)    
+        if ctx.author.id in data:
+            if data[ctx.author.id]["balance"] >= amount:
+                data[ctx.author.id]["balance"] -= amount
+                data[member.id]["balance"] += amount
+                write(data)
+                await ctx.send(f"Successfully paid {amount} to {member.mention}")
+            else:
+                await ctx.send("You don't have enough balance to pay")
+        else:
+            await ctx.send("You don't have an account")
+
 
 async def setup(bot):
-    await bot.add_cog(Payment(bot))
+    bot.add_cog(Payment(bot))
